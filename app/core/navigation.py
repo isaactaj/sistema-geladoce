0f49cb18be196e1c52ef2ce454b47a542feb0f65
joao marcelo <jmarcelobf12@gmail.com>
2026-02-20 11:21:44 -0300

    PaginaProdutos e PaginaReceitas na categoria estoque

--- app/core/navigation.py
+++ app/core/navigation.py
@@ -5,6 +5,8 @@ from app.pages.placeholder import PlaceholderPage
 from app.pages.relatorios.page import PaginaAdminRelatorios
 from app.pages.funcionarios.page import PaginaFuncionarios
 from app.pages.balcao.page import PaginaVendasBalcao
+from app.pages.estoque.produtos import PaginaProdutos
+from app.pages.estoque.receitas import PaginaReceitas
 from app.pages.estoque.page import PaginaEstoque
 
 
@@ -27,6 +29,8 @@ class Navigation(ctk.CTkFrame):
             "relatorios": PaginaAdminRelatorios,
             "funcionarios": PaginaFuncionarios,
             "balcao": PaginaVendasBalcao,
+            "produtos": PaginaProdutos,
+            "receitas": PaginaReceitas,
             "estoque": PaginaEstoque,
         }
 
